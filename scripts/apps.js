d9613646f481b74b695a35c007db991771c1e389
rkivesduh <menezes.e@escolar.ifrn.edu.br>
2026-08-19 18:00:30 -0300

    test

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,1 +1,13 @@
 alert ("olá mundo!");
+
+var nasc = 2009
+let nome = 'duds'
+const viva = true
+let altura = 1.62
+
+if (viva){
+    let saudacao = 'ola' + nome + "!";
+    let msg = "altura ${altura}n | idade ${2026-nasc}";
+    alert (saudacao)
+
+}
