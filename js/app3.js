90a5bd5feb2f81b97a37af212c069dcadfec10b1
rkivesduh <menezes.e@escolar.ifrn.edu.br>
2026-09-04 13:23:59 -0300

    new

---
+++ js/app3.js
@@ -0,0 +1,11 @@
+let dia = prompt("Escolha um dia da semana\n Sendo 1 domingo");
+dia = Number(dia);
+switch(dia){
+    case 1: alert("Você escolheu domingo"); break;
+    case 2: alert("Você escolheu segunda"); break;
+    case 3: alert("Você escolheu terça"); break;
+    case 4: alert("Você escolheu quarta"); break;
+    case 5: alert("Você escolheu quinta"); break;
+    case 6: alert("Você escolheu sexta"); break;
+    case 7: alert("Você escolheu sabado"); break;
+}
