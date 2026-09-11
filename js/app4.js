760edb01e585ead62012478e20bc7a8449deed04
rkivesduh <menezes.e@escolar.ifrn.edu.br>
2026-09-11 13:15:02 -0300

    .

--- js/app4.js
+++ js/app4.js
@@ -1,4 +1,13 @@
 let vezes = Number (prompt("Digite o numero de vezes:"));
 for (let i =1; i<vezes; i++){
-    alert(`Contei ${i} vez`);
+    if (vezes > 100){
+        alert("valor invalido, recarregue a pagina e digite um valor de 0 a 100");
+        break
+    }
+
+  alert ('Contei ${i} vez');
+  if (i%2!=0){ //é par
+    continue; //se par, continua = voltar
+  }
+    alert ('${i} é par');
 }
