bd1e06d85d41091c2c42465bdc14a2d9503813ca
rkivesduh <menezes.e@escolar.ifrn.edu.br>
2026-09-18 14:25:01 -0300

    agenda

---
+++ js/app1.js
@@ -0,0 +1,10 @@
+const amigos = ["Pedro", "ana", " Clara"];
+tam = amigos.length
+
+
+console.log(`Arranjo${amigos}`);
+console.log(`tamanho ${tam}`);
+console.log(`Primeiro ${amigos[0]}`);
+console.log(`Ultimo ${amigos[-1]}`);
+
+
