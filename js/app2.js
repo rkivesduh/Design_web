5ab3f42b0a78df68f0431d4727c08669341930c5
rkivesduh <menezes.e@escolar.ifrn.edu.br>
2026-09-23 17:56:28 -0300

    ex

--- js/app2.js
+++ js/app2.js
@@ -1,1 +1,35 @@
 const amigos = []
+const cadastro = document.getElementById ("cadastro");
+const nome = cadastro.nome;
+const nasc = cadastro.nasc;
+const whatsapp = cadastro.whatsapp;
+const lista =  document.getElementById("lista");
+cadastro.addEventListener("submit", function(e){
+    e.preventDefault();
+    let item = [nome.value, nasc.value, whatsapp.value];
+    amigos.unshift(item);
+    //limpa o formulario
+    cadastro.reset();
+    //atualiza lista
+    exibirlista();
+})
+
+function exibirlista(){
+    let itens = "";
+    for(let i = 0; i<amigos.length; i++){
+        let item = amigos[i]; //item = 
+        let remover = `<buttom onclick= "remover(${i})">Remover</buttom>`
+        let li = `<li>${item[0]} | ${item[1]} | ${item[2]}`;
+        itens = itens + li;
+    }
+    lista.innerHTML = itens;
+
+}
+
+function remover(i){
+    let item = amigos[i];
+    let check = confirm(`Deseja realmente excluir ${item[0]}?`);
+    if (check == true){
+        amigos.splice(i,1);
+    }
+}
