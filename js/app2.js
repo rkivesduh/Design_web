66f6c2a48bb7fb9ce0ea73aabe43641f43356ea5
rkivesduh <menezes.e@escolar.ifrn.edu.br>
2026-09-25 14:13:39 -0300

    pipopo

--- js/app2.js
+++ js/app2.js
@@ -4,12 +4,24 @@ const nome = cadastro.nome;
 const nasc = cadastro.nasc;
 const whatsapp = cadastro.whatsapp;
 const lista =  document.getElementById("lista");
+let editando = null;
 cadastro.addEventListener("submit", function(e){
     e.preventDefault();
-    let item = [nome.value, nasc.value, whatsapp.value];
-    amigos.unshift(item);
-    //limpa o formulario
-    cadastro.reset();
+let item = [nome.value, nasc.value, whatsapp.value];
+if (editando == null){
+    let check = amigos.find(item => item[0] == mome.value);
+    if (check == undefined){
+        amigos.unshift(item);
+        cadastro.requestFullscreen();
+    }else{
+        alert(`${nome.value} ja cadastrado`);
+    }
+}else{
+    let amigo = amigos [editando]
+    amigo[0] = nome.value;
+    amigo[0] = nasc.value;
+    amigo[0] = whatsapp.value;
+}
     //atualiza lista
     exibirlista();
 })
@@ -18,8 +30,8 @@ function exibirlista(){
     let itens = "";
     for(let i = 0; i<amigos.length; i++){
         let item = amigos[i]; //item = 
-        let remover = `<buttom onclick= "remover(${i})">Remover</buttom>`
-        let li = `<li>${item[0]} | ${item[1]} | ${item[2]}`;
+        let atualizar = `<buttom onclick= "remover(${i})">Remover</buttom>`
+        let li = `<li>${item[0]} | ${item[1]} | ${item[2]} ${atualizar} </li>`;
         itens = itens + li;
     }
     lista.innerHTML = itens;
@@ -32,4 +44,13 @@ function remover(i){
     if (check == true){
         amigos.splice(i,1);
     }
+    exibirlista();
+}
+
+function atualizar(i){
+    editando =i
+    let item = amigos[editando];
+    nome.value = item[0];
+    nasc.value = item[1];
+    whatsapp.value = item[2]; 
 }
